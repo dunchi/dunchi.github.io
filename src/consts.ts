3175b8c3f933ad6ac4857e65bae22922c5561c42
dunchi <pearl.dunchi@gmail.com>
2026-09-01 02:58:14 +0900

    feat(google): Gmail 작성창 형태의 메일 보내기 패널 추가

--- src/consts.ts
+++ src/consts.ts
@@ -16,4 +16,7 @@ export const LINKS = [
   { icon: 'tistory', label: '구 블로그', href: 'https://aiwcpd.tistory.com' },
   { icon: 'resume', label: '구 이력서', href: 'https://dunchi.github.io/docs/README.md.html' },
   { icon: 'youtube', label: 'YouTube', href: 'https://www.youtube.com/channel/UCXkAt4TGK0L1NF8gr-LuZEQ' },
+  // TODO: 실제 주소로 교체
+  { icon: 'kakao', label: '카카오톡 오픈프로필', href: 'https://open.kakao.com/o/CHANGE_ME' },
+  { icon: 'kakao', label: '카카오톡 1:1 채팅', href: 'https://qr.kakao.com/talk/CHANGE_ME' },
 ] as const;
